9cbd73d5b5f71a68db2e9bbbbb85a41b49313e99
Ernestas Ragainis <ernestas.ragainis@gmail.com>
2022-07-08 09:21:42 +0300

    Fixed albums files  and added posts.html and posts.js

--- albums.js
+++ albums.js
@@ -16,12 +16,15 @@ fetch('https://jsonplaceholder.typicode.com/albums')
       .then(res => res.json())
       .then(user => {
 
-        fetch(`https://jsonplaceholder.typicode.com/albums/${album.id}/photos?_limit=1`)
+        fetch(`https://jsonplaceholder.typicode.com/albums/${album.id}/photos?_limit=20`)
           .then(res => res.json())
           .then(photos => {
-            albumItem.innerHTML = `<h3><a href="./album.html?album_id=${album.id}&album_title=${album.title}&user_id=${album.userId}&user_name=${user.name}">${album.title}</a></h3>
+
+            
+            let randomIndex = Math.floor(Math.random() * photos.length)
+            albumItem.innerHTML = `<h3><a href="./album.html?album_id=${album.id}&album_title=${album.title}&user_id=${album.userId}&user_name=${user.name}">${album.title}</a> (${photos.length})</h3>
                                    <div>Album created by:<a href="./user.html?user_id=${user.id}">${user.name}</a> </div>
-                                   <img src="${photos[0].thumbnailUrl}">`;
+                                   <img src="${photos[randomIndex].thumbnailUrl}">`;
           })
       })
 
